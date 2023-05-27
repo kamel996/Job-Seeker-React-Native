ccc4d9fb4a75083cb829865dbe7891233032c07a
kamel996 <kamelnassar2828@gmail.com>
2023-05-27 14:56:33 +0300

    job details done

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,7 +20,7 @@ import {
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
-const tabs = ["About", "Qualifications", "Responsibilites"]
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 const JobDetails = () => {
   const params = useLocalSearchParams();
@@ -29,23 +29,34 @@ const JobDetails = () => {
     job_id: params.id,
   });
   const [refreshing, setRefreshing] = useState(false);
-  const [activeTab, setActiveTab] = useState(tabs[0])
-  const displayTabContent =() => {
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+  console.log(data);
+  const displayTabContent = () => {
     switch (activeTab) {
-        case "Qualifications":
-            return <Specifics
+      case "Qualifications":
+        return (
+          <Specifics
             title="Qualifications"
-            points={data[0].job_highlights?.qualifications ?? ['N/A']} />
-            case "About":
-                case "Responsibilites":
-            
-            break;
-            
-    
-        default:
-            break;
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+      case "About":
+        return (
+          <JobAbout 
+          info={data[0].job_description ?? "No data provided"}
+          />
+        )
+      case "Responsibilities":
+        return (
+          <Specifics
+            title="Responsibilities"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
+      default:
+        break;
     }
-  }
+  };
   const onRefresh = () => {};
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -68,7 +79,7 @@ const JobDetails = () => {
               handlePress={() => router.back()}
             />
           ),
-          headerTitle: ''
+          headerTitle: "",
         }}
       />
       <>
@@ -93,15 +104,15 @@ const JobDetails = () => {
                 location={data[0].job_country}
               />
               <JobTabs
-             tabs={tabs}
-             activeTab={activeTab}
-             setActiveTab={setActiveTab}
-              
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
               />
               {displayTabContent()}
             </View>
           )}
         </ScrollView>
+        <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results'} />
       </>
     </SafeAreaView>
   );
